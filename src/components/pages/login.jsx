5daea97edbf27b7368b7e468380cb7809e12cf4b
mukuru1 <kabengaedwin@gmail.com>
2025-12-26 11:20:20 +0200

    commit

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -32,6 +33,13 @@ export default function Login() {
       <button className="w-full bg-blue-500 text-white p-2">
         Login
       </button>
+
+      <Link
+  to="/register"
+  className="block mt-4 text-center text-sm text-blue-600"
+>
+  Don't have an account? Register
+</Link>
     
     </form>
   );
