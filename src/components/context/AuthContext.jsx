cbe8395e70ce090bcdb5bc4f2d3eaa0193cb05dd
mukuru1 <kabengaedwin@gmail.com>
2025-12-28 22:48:20 +0200

    commit

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { INITIAL_USERS } from "../data/seed";
+import { INITIAL_USERS } from "../../users/data";
 
 const AuthContext = createContext(null);
 
